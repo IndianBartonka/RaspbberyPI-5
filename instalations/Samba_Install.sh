37b373b0f89fc6c7b3ed1bbaa20680f525fa1e56
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2025-08-02 12:08:01 +0200

    Update Samba_Install.sh

--- instalations/Samba_Install.sh
+++ instalations/Samba_Install.sh
@@ -1,65 +1,34 @@
 #!/bin/bash
-# Autorem: IndianBartonka – automatyczna instalacja Samby z konfiguracją
+# Autorem jest IndianBartonka
 
-# Sprawdzenie uprawnień
+# Sprawdzanie uprawnień
 if [[ $EUID -ne 0 ]]; then
-   echo -e "\n\e[31mUruchom ten skrypt jako root (sudo).\e[0m"
+   echo ""
+   echo -e "\e[31mUruchom ten skrypt jako root lub za pomocą sudo.\e[0m"
    exit 1
 fi
 
-echo -e "\nAktualizowanie systemu..."
+echo ""
+echo "Aktualizowanie systemu..."
 apt update && apt upgrade -y
 
-echo -e "\nInstalowanie Samby..."
+echo ""
+echo "Instaluje Sambe..."
 apt install samba -y
 
-# Tworzenie katalogu współdzielonego
-SHARE_PATH="/srv/samba/nas"
-mkdir -p "$SHARE_PATH"
+echo ""
+echo "Za 5 sekund zostanie otwarty plik konfiguracyjny samby do edycji..."
+sleep 5
+nano /etc/samba/smb.conf
 
-# Zapytaj o użytkownika
 echo ""
 read -p "Podaj nazwę użytkownika do Samby: " userName
-
-# Dodanie użytkownika systemowego, jeśli nie istnieje
-if ! id "$userName" &>/dev/null; then
-    adduser "$userName"
-fi
-
-# Ustaw hasło do Samby
+adduser "$userName"
 smbpasswd -a "$userName"
 
-# Nadanie uprawnień do folderu
-chown "$userName:$userName" "$SHARE_PATH"
-chmod -R 775 "$SHARE_PATH"
-
-# Tworzenie domyślnego pliku konfiguracyjnego Samby
-cat > /etc/samba/smb.conf <<EOF
-[global]
-   workgroup = WORKGROUP
-   server string = RaspberryPi Samba Server
-   map to guest = Bad User
-   log file = /var/log/samba/log.%m
-   max log size = 1000
-   logging = file
-   server role = standalone server
-   usershare allow guests = no
-   unix password sync = yes
-   security = user
-   passdb backend = tdbsam
-
-[nas]
-   comment = Udostępniony folder NAS
-   path = $SHARE_PATH
-   browseable = yes
-   writable = yes
-   valid users = $userName
-   create mask = 0660
-   directory mask = 0771
-EOF
-
-# Restart usługi Samba
+echo ""
+echo "Restartowanie Samby..."
 systemctl restart smbd
 
-echo -e "\n\e[32mSamba skonfigurowana!\e[0m"
-echo -e "Udział: \\\\$(hostname -I | awk '{print $1}')\\nas"
+echo ""
+echo -e "\e[32mSamba została zainstalowana i skonfigurowana.\e[0m"
