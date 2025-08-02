05db513d0706742d5100867b481977f3d5c8212e
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2025-08-02 12:03:41 +0200

    Update Install_Important.sh

--- instalations/Install_Important.sh
+++ instalations/Install_Important.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-set -e  # zatrzymaj przy błędzie
+
+# Sprawdzanie uprawnień
+if [[ $EUID -ne 0 ]]; then
+   echo ""
+   echo -e "\e[31mUruchom ten skrypt jako root lub za pomocą sudo.\e[0m"
+   exit 1
+fi
 
 echo ""
 echo "Aktualizowanie systemu..."
