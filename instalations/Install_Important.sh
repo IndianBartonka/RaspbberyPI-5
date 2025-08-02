355aaef3a2913ddda72c514e5e61292f56251f77
IndianBartonka <68228599+IndianBartonka@users.noreply.github.com>
2025-08-02 11:50:15 +0200

    Instaluje to co najwaznjejsze

---
+++ instalations/Install_Important.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e  # zatrzymaj przy błędzie
+
+echo "Instaluję JDK 23..."
+curl -s https://raw.githubusercontent.com/IndianBartonka/RaspbberyPI-5/main/instalations/JDK23_Install.sh | sudo bash
+
+echo "Wykonuję instalację podstawową..."
+curl -s https://raw.githubusercontent.com/IndianBartonka/RaspbberyPI-5/main/instalations/Basic.sh | sudo bash
+
+echo "Ustawiam język polski..."
+curl -s https://raw.githubusercontent.com/IndianBartonka/RaspbberyPI-5/main/instalations/Polish_Lang.sh | sudo bash
+
+echo "Wszystko gotowe!"
